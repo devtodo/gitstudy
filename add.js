0e46de862ea1796488dd920294ab37d327b2b3c1
devtodo <simulat@naver.com>
2021-12-11 20:38:29 +0900

    apply code prettier

--- add.js
+++ add.js
@@ -1,17 +1,17 @@
 function add(a, b) {
-    return a+b;
+  return a + b;
 }
 
 function sub(a, b) {
-    return a-b;
+  return a - b;
 }
 
 function mul(a, b) {
-    return a*b;
+  return a * b;
 }
 
 function div(a, b) {
-    return a/b;
+  return a / b;
 }
 
-module.exports = {add, sub, mul, div};
+module.exports = { add, sub, mul, div };
--- add.test.js
+++ add.test.js
@@ -1,17 +1,17 @@
 const { add, sub, mul, div } = require("./add");
 
 test("2+5 = 7 이 맞남?", () => {
-    expect(add(2,5)).toBe(7);
+  expect(add(2, 5)).toBe(7);
 });
 
 test("3*5 = 15 이 맞남?", () => {
-    expect(mul(3,5)).toBe(15);
+  expect(mul(3, 5)).toBe(15);
 });
 
 test("4-2 = 2 이 맞남?", () => {
-    expect(sub(4,2)).toBe(2);
+  expect(sub(4, 2)).toBe(2);
 });
 
 test("8/2 = 4 이 맞남?", () => {
-    expect(div(8,2)).toBe(4);
+  expect(div(8, 2)).toBe(4);
 });
