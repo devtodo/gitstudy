c5e08c3ddfe8d70ed5a4df0a649c49b40279edba
devtodo <simulat@naver.com>
2021-12-11 19:46:20 +0900

    added div function test

--- add.test.js
+++ add.test.js
@@ -1,4 +1,4 @@
-const { add, sub, mul } = require("./add");
+const { add, sub, mul, div } = require("./add");
 
 test("2+5 = 7 이 맞남?", () => {
     expect(add(2,5)).toBe(7);
@@ -10,4 +10,8 @@ test("3*5 = 15 이 맞남?", () => {
 
 test("4-2 = 2 이 맞남?", () => {
     expect(sub(4,2)).toBe(2);
+});
+
+test("8/2 = 4 이 맞남?", () => {
+    expect(div(8,2)).toBe(4);
 });
